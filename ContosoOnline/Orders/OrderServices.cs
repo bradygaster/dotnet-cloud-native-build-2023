a6881f699a40741b99d6582c5d9fa975816e0f54
bradygaster <bradyg@microsoft.com>
2023-05-08 23:40:35 -0700

    real fake order now

--- ContosoOnline/Orders/OrderServices.cs
+++ ContosoOnline/Orders/OrderServices.cs
@@ -10,7 +10,18 @@
     {
         public Task<Order[]> GetOrdersAsync()
         {
-            throw new NotImplementedException();
+            var fakeOrders = new List<Order>();
+            fakeOrders.AddRange(new Order[]
+            {
+                new Order(new CartItem[] 
+                { 
+                    new CartItem("1", 2, 1.99m), 
+                    new CartItem("2", 1, 19.99m), 
+                    new CartItem("3", 4, 4.99m), 
+                    new CartItem("4", 4, 5.99m),
+                }, DateTime.Now),
+            });
+            return Task.FromResult(fakeOrders.ToArray());
         }
 
         public Task<bool> SaveOrderAsync(Order order)

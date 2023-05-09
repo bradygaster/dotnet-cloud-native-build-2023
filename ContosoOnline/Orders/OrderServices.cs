d7732e43cd5a4f1036476e9f488feb9640169857
bradygaster <bradyg@microsoft.com>
2023-05-08 23:35:17 -0700

    getting wired up

---
+++ ContosoOnline/Orders/OrderServices.cs
@@ -0,0 +1,21 @@
+﻿namespace Orders
+{
+    public interface IOrderService
+    {
+        public Task<Order[]> GetOrdersAsync();
+        public Task<bool> SaveOrderAsync(Order order);
+    }
+
+    public class FakeOrderService : IOrderService
+    {
+        public Task<Order[]> GetOrdersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveOrderAsync(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ContosoOnline/Orders/Program.cs
+++ ContosoOnline/Orders/Program.cs
@@ -1,35 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
+using Orders;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<IOrderService, FakeOrderService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-}
+app.MapGet("/orders", async (IOrderService service) =>
+    await service.GetOrdersAsync()
+    );
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapPost("/orders", async (Order order, IOrderService service) =>
+    await service.SaveOrderAsync(order)
+    );
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public record CartItem(string productId, int quantity = 1, decimal pricePerItem = 0)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public record Order(CartItem[] cart, DateTime orderedAt)
+{
 }

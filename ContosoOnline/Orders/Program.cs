89943f4c39e3252ac60504779b4c99874d52a682
bradygaster <bradyg@microsoft.com>
2023-05-09 15:12:32 -0700

    added prom and graf. but there is no data. yet. i'm optimistic.

--- ContosoOnline/Orders/Program.cs
+++ ContosoOnline/Orders/Program.cs
@@ -1,9 +1,24 @@
+using OpenTelemetry.Metrics;
 using Orders;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IOrderService, FakeOrderService>();
 
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder
+            .AddView("request-duration", new ExplicitBucketHistogramConfiguration
+                {
+                    Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+                })
+            .AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel")
+            .AddPrometheusExporter();
+    });
+
 var app = builder.Build();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 app.MapGet("/orders", async (IOrderService service) =>
     await service.GetOrdersAsync()
--- ContosoOnline/Products/Program.cs
+++ ContosoOnline/Products/Program.cs
@@ -1,10 +1,24 @@
+using OpenTelemetry.Metrics;
 using Products;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IProductService, FakeProductService>();
 builder.Services.AddGrpc();
 
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder.AddView("request-duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+            });
+        builder.AddPrometheusExporter();
+        builder.AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel");
+    });
+
 var app = builder.Build();
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 app.MapGrpcService<ProductsGrpcService>();
 
 app.Run();

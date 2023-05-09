fa78b1ed010563a5934dd2729a3c4d12787b3862
bradygaster <bradyg@microsoft.com>
2023-05-09 16:24:16 -0700

    more diagnostics data being pumped out of the app. but to where?

--- ContosoOnline/Orders/Program.cs
+++ ContosoOnline/Orders/Program.cs
@@ -1,4 +1,6 @@
 using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Orders;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,12 +10,33 @@ builder.Services.AddOpenTelemetry()
     .WithMetrics(builder =>
     {
         builder
+            .AddAspNetCoreInstrumentation()
+            .AddRuntimeInstrumentation()
+            .AddEventCountersInstrumentation(c => {
+                c.AddEventSources(
+                    "Microsoft.AspNetCore.Hosting",
+                    "Microsoft-AspNetCore-Server-Kestrel",
+                    "System.Net.Http",
+                    "System.Net.Sockets",
+                    "System.Net.NameResolution",
+                    "System.Net.Security");
+            })
             .AddView("request-duration", new ExplicitBucketHistogramConfiguration
                 {
                     Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
                 })
             .AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel")
             .AddPrometheusExporter();
+    })
+    .WithTracing(tracing => {
+        tracing
+            .AddAspNetCoreInstrumentation()
+            .SetResourceBuilder(ResourceBuilder.CreateDefault()
+                .AddService(serviceName: "Orders", serviceVersion: "1.0"));
+
+        tracing.AddZipkinExporter(zipkin => {
+            zipkin.Endpoint = new Uri("http://zipkin:9411/api/v2/spans");
+        });
     });
 
 var app = builder.Build();
--- ContosoOnline/Products/Program.cs
+++ ContosoOnline/Products/Program.cs
@@ -1,4 +1,6 @@
 using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Products;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,13 +10,34 @@ builder.Services.AddGrpc();
 builder.Services.AddOpenTelemetry()
     .WithMetrics(builder =>
     {
-        builder.AddView("request-duration",
-            new ExplicitBucketHistogramConfiguration
+        builder
+            .AddAspNetCoreInstrumentation()
+            .AddRuntimeInstrumentation()
+            .AddEventCountersInstrumentation(c => {
+                c.AddEventSources(
+                    "Microsoft.AspNetCore.Hosting",
+                    "Microsoft-AspNetCore-Server-Kestrel",
+                    "System.Net.Http",
+                    "System.Net.Sockets",
+                    "System.Net.NameResolution",
+                    "System.Net.Security");
+            })
+            .AddView("request-duration", new ExplicitBucketHistogramConfiguration
             {
                 Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
-            });
-        builder.AddPrometheusExporter();
-        builder.AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel");
+            })
+            .AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel")
+            .AddPrometheusExporter();
+    })
+    .WithTracing(tracing => {
+        tracing
+            .AddAspNetCoreInstrumentation()
+            .SetResourceBuilder(ResourceBuilder.CreateDefault()
+                .AddService(serviceName: "Products", serviceVersion: "1.0"));
+
+        tracing.AddZipkinExporter(zipkin => {
+            zipkin.Endpoint = new Uri("http://zipkin:9411/api/v2/spans");
+        });
     });
 
 var app = builder.Build();

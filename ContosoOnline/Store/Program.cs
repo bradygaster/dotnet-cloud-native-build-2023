1668f5e24d32f9f2532af555f7e193a0400b68ff
David Fowler <davidfowl@gmail.com>
2023-05-19 02:12:50 -0700

    Added the standard reslience handlers to the front end

--- ContosoOnline/Store/Program.cs
+++ ContosoOnline/Store/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Http.Resilience;
 using MudBlazor.Services;
 using Store;
 
@@ -10,14 +11,16 @@ builder.Services.AddGrpcClient<Products.Products.ProductsClient>(c =>
     var backendUrl = builder.Configuration["PRODUCTS_URL"] ?? throw new InvalidOperationException("PRODUCTS_URL is not set");
 
     c.Address = new(backendUrl);
-});
+})
+.AddStandardResilienceHandler();
 
 builder.Services.AddHttpClient<OrderServiceClient>(c =>
 {
     var url = builder.Configuration["ORDERS_URL"] ?? throw new InvalidOperationException("ORDERS_URL is not set");
 
     c.BaseAddress = new(url);
-});
+})
+.AddStandardResilienceHandler();
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();

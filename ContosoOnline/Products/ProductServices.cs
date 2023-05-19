9b9964d6acea95c22dc10f2d518e44a1e343bc43
David Fowler <davidfowl@gmail.com>
2023-05-18 23:35:25 -0700

    More primary ctors

--- ContosoOnline/Products/ProductServices.cs
+++ ContosoOnline/Products/ProductServices.cs
@@ -7,24 +7,16 @@ public interface IProductService
     public Task<bool> SubtractInventory(string productId, int quantityNeeded);
 }
 
-public class FakeProductService : IProductService
+public class FakeProductService(Logger<FakeProductService> logger) : IProductService
 {
-    private List<Product> _fakeOrders = new();
-    private ILogger<FakeProductService> _logger;
-
-    public FakeProductService(ILogger<FakeProductService> logger)
+    private List<Product> _fakeOrders = new()
     {
-        _logger = logger;
-
-        _fakeOrders.AddRange(new Product[]
-        {
-            new Product { Name = "Apple", Price = 0.99f, ItemsInStock = 25, ProductId = "01" },
-            new Product { Name = "Banana", Price = 0.99f, ItemsInStock = 25, ProductId = "02" },
-            new Product { Name = "Orange", Price = 0.99f, ItemsInStock = 25, ProductId = "03" },
-            new Product { Name = "Pear", Price = 0.99f, ItemsInStock = 25, ProductId = "04" },
-            new Product { Name = "Pineapple", Price = 0.99f, ItemsInStock = 25, ProductId = "05" }
-        });
-    }
+        new () { Name = "Apple", Price = 0.99f, ItemsInStock = 25, ProductId = "01" },
+        new () { Name = "Banana", Price = 0.99f, ItemsInStock = 25, ProductId = "02" },
+        new () { Name = "Orange", Price = 0.99f, ItemsInStock = 25, ProductId = "03" },
+        new () { Name = "Pear", Price = 0.99f, ItemsInStock = 25, ProductId = "04" },
+        new () { Name = "Pineapple", Price = 0.99f, ItemsInStock = 25, ProductId = "05" }
+    };
 
     public Task<Product[]?> GetProductsAsync()
     {
@@ -34,35 +26,35 @@ public class FakeProductService : IProductService
 
     public Task<bool> CheckProductInventoryAsync(string productId, int quantityNeeded)
     {
-        _logger.LogInformation("Checking inventory for {ProductId} with quantity {QuantityNeeded}", productId, quantityNeeded);
+        logger.LogInformation("Checking inventory for {ProductId} with quantity {QuantityNeeded}", productId, quantityNeeded);
 
         bool isEnoughInStock = _fakeOrders?.FirstOrDefault(p => p.ProductId == productId)?.ItemsInStock >= quantityNeeded;
 
-        _logger.LogInformation("Are enough items in stock for Product {ProductId}: {IsEnoughInStock}", productId, isEnoughInStock);
+        logger.LogInformation("Are enough items in stock for Product {ProductId}: {IsEnoughInStock}", productId, isEnoughInStock);
 
         return Task.FromResult(isEnoughInStock);
     }
 
     public Task<bool> SubtractInventory(string productId, int quantityNeeded)
     {
-        _logger.LogInformation("Subtracting {QuantityNeeded} from {ProductId} inventory", quantityNeeded, productId);
+        logger.LogInformation("Subtracting {QuantityNeeded} from {ProductId} inventory", quantityNeeded, productId);
 
         var product = _fakeOrders.FirstOrDefault(p => p.ProductId == productId);
         if (product != null)
         {
-            _logger.LogInformation("Found product {Name} with {ItemsInStock} in stock", product.Name, product.ItemsInStock);
+            logger.LogInformation("Found product {Name} with {ItemsInStock} in stock", product.Name, product.ItemsInStock);
 
             product.ItemsInStock -= quantityNeeded;
             _fakeOrders.RemoveAll(p => p.ProductId == productId);
             _fakeOrders.Add(product);
 
-            _logger.LogInformation("Subtracted {QuantityNeeded} from {Name} inventory. New inventory is {ItemsInStock}", quantityNeeded, product.Name, product.ItemsInStock);
+            logger.LogInformation("Subtracted {QuantityNeeded} from {Name} inventory. New inventory is {ItemsInStock}", quantityNeeded, product.Name, product.ItemsInStock);
 
             return Task.FromResult(true);
         }
         else
         {
-            _logger.LogError("Unable to find product with id {ProductId}", productId);
+            logger.LogError("Unable to find product with id {ProductId}", productId);
 
             return Task.FromResult(false);
         }
--- ContosoOnline/Products/Program.cs
+++ ContosoOnline/Products/Program.cs
@@ -1,9 +1,9 @@
 using Products;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddSingleton<IProductService, FakeProductService>();
 builder.Services.AddGrpc();
-
 builder.Services.AddObservability("Products");
 
 var app = builder.Build();

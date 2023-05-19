fea524636a06c9b70aa73bc4ff7ca12276f418d6
David Fowler <davidfowl@gmail.com>
2023-05-19 02:06:01 -0700

    Added basic reslience to the order processing

--- ContosoOnline/OrderProcessor/Program.cs
+++ ContosoOnline/OrderProcessor/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.Extensions.Http.Resilience;
 using OrderProcessor;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+var resilienceSection = builder.Configuration.GetSection("HttpStandardResilienceOptions");
 
 builder.Services.AddSingleton<ProductServiceClient>();
 builder.Services.AddGrpcClient<Products.Products.ProductsClient>(c =>
@@ -8,14 +11,15 @@ builder.Services.AddGrpcClient<Products.Products.ProductsClient>(c =>
     var backendUrl = builder.Configuration["PRODUCTS_URL"] ?? throw new InvalidOperationException("PRODUCTS_URL is not set");
 
     c.Address = new(backendUrl);
-});
+})
+.AddStandardResilienceHandler(resilienceSection);
 
 builder.Services.AddHttpClient<OrderServiceClient>(c =>
 {
     var url = builder.Configuration["ORDERS_URL"] ?? throw new InvalidOperationException("ORDERS_URL is not set");
-
     c.BaseAddress = new(url);
-});
+})
+.AddStandardResilienceHandler(resilienceSection);
 
 builder.Services.AddObservability("OrderProcessor", builder.Configuration, tracing =>
 {

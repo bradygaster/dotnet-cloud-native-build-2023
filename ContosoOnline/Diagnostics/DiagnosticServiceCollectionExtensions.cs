081d8b77b80b72566b08aeeba97136618d3b2aac
bradygaster <bradyg@microsoft.com>
2023-09-15 10:08:30 -0700

    tweaks to not EXPECT Azure Monitor - if the connection string is there it'll work, if it isn't there, it will stop breaking. Also commented out resiliency handler in Worker service, as I think post-build this API may have changed as it's breaking with the worker. cc @davidfowl.

--- ContosoOnline/Diagnostics/DiagnosticServiceCollectionExtensions.cs
+++ ContosoOnline/Diagnostics/DiagnosticServiceCollectionExtensions.cs
@@ -7,53 +7,60 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class DiagnosticServiceCollectionExtensions
 {
-    public static IServiceCollection AddObservability(this IServiceCollection services, 
+    public static IServiceCollection AddObservability(this IServiceCollection services,
         string serviceName,
         IConfiguration configuration,
         Action<TracerProviderBuilder>? tracerBuilderAction = null)
     {
         var resource = ResourceBuilder.CreateDefault().AddService(serviceName: serviceName, serviceVersion: "1.0");
 
-        services
-            .AddOpenTelemetry()
-            .UseAzureMonitor()
-                .WithMetrics(metrics =>
-                {
-                    metrics
-                        .SetResourceBuilder(resource)
-                        .AddRuntimeInstrumentation()
+        // did they ask for azure monitor?
+        var otelBuilder = services.AddOpenTelemetry();
+
+        if (!string.IsNullOrEmpty(configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]))
+        {
+            otelBuilder.UseAzureMonitor();
+        }
+
+        // add the metrics and traces
+        otelBuilder
+            .WithMetrics(metrics =>
+            {
+                metrics
+                    .SetResourceBuilder(resource)
+                    .AddRuntimeInstrumentation()
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddEventCountersInstrumentation(c =>
+                    {
+                        c.AddEventSources(
+                            "Microsoft.AspNetCore.Hosting",
+                            "Microsoft-AspNetCore-Server-Kestrel",
+                            "System.Net.Http",
+                            "System.Net.Sockets");
+                    })
+                    .AddView("request-duration", new ExplicitBucketHistogramConfiguration
+                    {
+                        Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+                    })
+                    .AddMeter("Microsoft.AspNetCore.Hosting",
+                        "Microsoft.AspNetCore.Server.Kestrel")
+                    .AddPrometheusExporter();
+            })
+            .WithTracing(tracing =>
+            {
+                tracing.SetResourceBuilder(resource)
                         .AddAspNetCoreInstrumentation()
                         .AddHttpClientInstrumentation()
-                        .AddEventCountersInstrumentation(c =>
+                        .AddGrpcClientInstrumentation()
+                        .AddZipkinExporter(zipkin =>
                         {
-                            c.AddEventSources(
-                                "Microsoft.AspNetCore.Hosting",
-                                "Microsoft-AspNetCore-Server-Kestrel",
-                                "System.Net.Http",
-                                "System.Net.Sockets");
-                        })
-                        .AddView("request-duration", new ExplicitBucketHistogramConfiguration
-                        {
-                            Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
-                        })
-                        .AddMeter("Microsoft.AspNetCore.Hosting", 
-                            "Microsoft.AspNetCore.Server.Kestrel")
-                        .AddPrometheusExporter();
-                })
-                .WithTracing(tracing =>
-                {
-                    tracing.SetResourceBuilder(resource)
-                           .AddAspNetCoreInstrumentation()
-                           .AddHttpClientInstrumentation()
-                           .AddGrpcClientInstrumentation()
-                           .AddZipkinExporter(zipkin =>
-                           {
-                               var zipkinUrl = configuration["ZIPKIN_URL"] ?? "http://localhost:9411";
-                               zipkin.Endpoint = new Uri($"{zipkinUrl}/api/v2/spans");
-                           });
+                            var zipkinUrl = configuration["ZIPKIN_URL"] ?? "http://localhost:9411";
+                            zipkin.Endpoint = new Uri($"{zipkinUrl}/api/v2/spans");
+                        });
 
-                    tracerBuilderAction?.Invoke(tracing);
-                });
+                tracerBuilderAction?.Invoke(tracing);
+            });
 
         return services;
     }

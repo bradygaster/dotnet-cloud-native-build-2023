ea49303a0b2a7205ff552447f7632c1074fcbd76
bradygaster <bradyg@microsoft.com>
2024-01-18 22:20:44 -0800

    simplified products grpc project

--- ContosoOnline/Products/Program.cs
+++ ContosoOnline/Products/Program.cs
@@ -2,13 +2,14 @@ using Products;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<IProductService, FakeProductService>();
+builder.AddServiceDefaults();
 builder.Services.AddGrpc();
+builder.Services.AddSingleton<IProductService, FakeProductService>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Products");
-
 app.MapGrpcService<ProductsGrpcService>();
+
+app.MapDefaultEndpoints();
 
 app.Run();

7704c579916d3b80057911dbfe58c5f5267eb017
bradygaster <bradyg@microsoft.com>
2024-01-18 22:21:36 -0800

    added orchestration

---
+++ ContosoOnline/ContosoOnline.AppHost/Program.cs
@@ -0,0 +1,18 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var ordersDb = builder.AddPostgres("ordersDb");
+
+var products = builder.AddProject<Projects.Products>("products");
+
+var ordersApi = builder.AddProject<Projects.Orders>("orders")
+                         .WithReference(ordersDb);
+
+var store = builder.AddProject<Projects.Store>("store")
+                   .WithReference(products)
+                   .WithReference(ordersApi);
+
+builder.AddProject<Projects.OrderProcessor>("orderprocessor")
+       .WithReference(ordersApi)
+       .WithReference(products);
+
+builder.Build().Run();
